60038d51700e324e4da3c679f556c2aa545faa05
MIGUEL\migue <miguelcocohernandez5@gmail.com>
2025-05-26 00:05:05 +0200

    feat: Flujo de registro de entrenador premium hecho y funcionando

--- src/app/components/login/tarjeta-suscripcion/tarjeta-suscripcion.component.ts
+++ src/app/components/login/tarjeta-suscripcion/tarjeta-suscripcion.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthService, RegistroAuth } from '../../../services/authService/auth.service';
+import { firstValueFrom } from 'rxjs';
+import { RegistroTrainer, TrainerService } from '../../../services/trainerService/trainer.service';
+import { PaymentService } from '../../../services/paymentService/payment.service';
 
 @Component({
   selector: 'app-tarjeta-suscripcion',
@@ -17,11 +21,45 @@ export class TarjetaSuscripcionComponent {
   @Input() perfilDestacado: string = '';
   @Input() textoBoton: string = '';
 
-  suscripcionPago() {
+  constructor(private authService: AuthService, private trainerService: TrainerService, private paymentService: PaymentService) {}
 
+  async suscripcionPago() {
+    const datosDelForm = JSON.parse(sessionStorage.getItem('registroTrainer')!)
+
+    const datosRegistroAuth: RegistroAuth = {
+      nombreusuario: datosDelForm.nombreUsuario,
+      contraseña: datosDelForm.contraseña,
+      email: datosDelForm.email,
+      rol: 'TRAINER'
+    }
+
+    const datosRegistroTrainer: RegistroTrainer = {
+      nombre: datosDelForm.nombre,
+      apellidos: datosDelForm.apellidos,
+      tfno: datosDelForm.telefono,
+      bio: datosDelForm.bio
+    }
+
+    try {
+      //Registro en el auth y te da el token
+      const authResponse = await firstValueFrom(this.authService.registrarUsuario(datosRegistroAuth))
+      const token = authResponse.token
+
+      //Registro en el trainer
+      await firstValueFrom(this.trainerService.registrarTrainer(datosRegistroTrainer, token))
+
+      //Redirigir a la pantalla de pago
+      const stripeResponse = await firstValueFrom(this.paymentService.suscripcionPremium(token))
+      const url = stripeResponse.url
+
+      window.location.href = url
+
+    } catch (error) {
+      console.error('Error al registrar el usuario:', error);
+    }
   }
 
-  suscripcionGratuita() {
+  async suscripcionGratuita() {
 
   }
 }
