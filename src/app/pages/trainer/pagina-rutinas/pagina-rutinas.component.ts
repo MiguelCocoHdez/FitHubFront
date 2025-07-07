011e8aca43aa58a4a7946547680272c98b4c1195
MIGUEL\migue <miguelcocohernandez5@gmail.com>
2025-07-08 00:03:37 +0200

    feat: Llamada a la api de rutinas funcional

--- src/app/pages/trainer/pagina-rutinas/pagina-rutinas.component.ts
+++ src/app/pages/trainer/pagina-rutinas/pagina-rutinas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Trainer } from '../../../services/trainerService/trainer.service';
 import { SidebarDashboardTrainerComponent } from "../../../components/trainer/sidebar-dashboard-trainer/sidebar-dashboard-trainer.component";
+import { Rutina, RutinasService } from '../../../services/rutinasService/rutinas.service';
 
 @Component({
   selector: 'app-pagina-rutinas',
@@ -11,8 +12,22 @@ import { SidebarDashboardTrainerComponent } from "../../../components/trainer/si
 export class PaginaRutinasComponent implements OnInit {
 
   trainer!: Trainer
+  rutinas!: Rutina[]
+
+  constructor(private rutinasService: RutinasService) {}
 
   ngOnInit(): void {
-    this.trainer = JSON.parse(localStorage.getItem('trainer') || '{}');
+    this.trainer = JSON.parse(localStorage.getItem('trainer') || '{}')
+    const token = localStorage.getItem('token') || ''
+
+    this.rutinasService.verRutinasTrainer(this.trainer.id, token).subscribe({
+      next: (data) => {
+        this.rutinas = data
+        console.log(this.rutinas)
+      },
+      error: (error) => {
+        console.log('Error al obtener las rutinas del trainer:', error)
+      }
+    })
   }
 }
