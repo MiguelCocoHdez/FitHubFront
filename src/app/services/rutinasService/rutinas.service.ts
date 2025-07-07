dd958d3bde09e14052e7a62858954164fe496ea2
MIGUEL\migue <miguelcocohernandez5@gmail.com>
2025-07-07 22:24:36 +0200

    feat: Añadidos atributos a rutinas

--- src/app/services/rutinasService/rutinas.service.ts
+++ src/app/services/rutinasService/rutinas.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
 
 export interface Rutina {
-
+  id: number
+  nombre: string
+  descripcion: string
+  nivel: string
+  categoria: string
+  duracionMins: number
+  ejercicios: Ejercicio[]
+  fechaCreacion: Date
+  ultimaActualizacion: Date
+  trainerId: number
 }
 
 export interface Ejercicio {
-  
+  id: number
+  nombre: string
+  descripcion: string
+  series: number
+  repeticionesMin: number
+  repeticionesMax: number
 }
 
 @Injectable({
