7581da2b8e4cc1488c1f739b1c88db4426d46a63
MIGUEL\migue <miguelcocohernandez5@gmail.com>
2025-07-08 01:28:16 +0200

    feat: Tarjeta rutina completa + responsive

--- src/app/components/trainer/cards-rutinas/cards-rutinas.component.ts
+++ src/app/components/trainer/cards-rutinas/cards-rutinas.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Rutina } from '../../../services/rutinasService/rutinas.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cards-rutinas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cards-rutinas.component.html',
   styleUrl: './cards-rutinas.component.css'
 })
 export class CardsRutinasComponent {
 
+  @Input() rutina!: Rutina
 }
--- src/app/pages/trainer/pagina-rutinas/pagina-rutinas.component.ts
+++ src/app/pages/trainer/pagina-rutinas/pagina-rutinas.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Trainer } from '../../../services/trainerService/trainer.service';
 import { SidebarDashboardTrainerComponent } from "../../../components/trainer/sidebar-dashboard-trainer/sidebar-dashboard-trainer.component";
 import { Rutina, RutinasService } from '../../../services/rutinasService/rutinas.service';
+import { CardsRutinasComponent } from "../../../components/trainer/cards-rutinas/cards-rutinas.component";
 
 @Component({
   selector: 'app-pagina-rutinas',
-  imports: [SidebarDashboardTrainerComponent],
+  imports: [SidebarDashboardTrainerComponent, CardsRutinasComponent],
   templateUrl: './pagina-rutinas.component.html',
   styleUrl: './pagina-rutinas.component.css'
 })
@@ -14,7 +15,7 @@ export class PaginaRutinasComponent implements OnInit {
   trainer!: Trainer
   rutinas!: Rutina[]
 
-  constructor(private rutinasService: RutinasService) {}
+  constructor(private rutinasService: RutinasService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.trainer = JSON.parse(localStorage.getItem('trainer') || '{}')
@@ -24,6 +25,7 @@ export class PaginaRutinasComponent implements OnInit {
       next: (data) => {
         this.rutinas = data
         console.log(this.rutinas)
+        this.cdr.detectChanges()
       },
       error: (error) => {
         console.log('Error al obtener las rutinas del trainer:', error)
