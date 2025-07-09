08543bff8124609cd2a05a7105a4e585cf0c17ab
MIGUEL\migue <miguelcocohernandez5@gmail.com>
2025-07-10 00:51:53 +0200

    feat: Modal se abre al darle al boton, falta hacer el contenido del modal

--- src/app/components/trainer/cards-rutinas/cards-rutinas.component.ts
+++ src/app/components/trainer/cards-rutinas/cards-rutinas.component.ts
@@ -1,14 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { Rutina } from '../../../services/rutinasService/rutinas.service';
 import { CommonModule } from '@angular/common';
+import { ModalAvisoEliminarRutinaComponent } from "../modal-aviso-eliminar-rutina/modal-aviso-eliminar-rutina.component";
 
 @Component({
   selector: 'app-cards-rutinas',
-  imports: [CommonModule],
+  imports: [CommonModule, ModalAvisoEliminarRutinaComponent],
   templateUrl: './cards-rutinas.component.html',
   styleUrl: './cards-rutinas.component.css'
 })
 export class CardsRutinasComponent {
 
   @Input() rutina!: Rutina
+
+  mostrarModal = signal(false)
+
+  accionModal() {
+    this.mostrarModal.update((valor) => !valor)
+    console.log('Modal toggled:', this.mostrarModal());
+  }
 }
