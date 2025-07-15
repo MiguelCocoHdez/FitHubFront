7f9faa27a6f422e27baa8e0242f4fb1e5c457348
MIGUEL\migue <miguelcocohernandez5@gmail.com>
2025-07-16 00:21:46 +0200

    fix: Boton de crear rutina hecho componente independiente

---
+++ src/app/components/trainer/boton-crear-rutina/boton-crear-rutina.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-crear-rutina',
+  imports: [],
+  templateUrl: './boton-crear-rutina.component.html',
+  styleUrl: './boton-crear-rutina.component.css'
+})
+export class BotonCrearRutinaComponent {
+
+}
--- src/app/pages/trainer/pagina-rutinas/pagina-rutinas.component.ts
+++ src/app/pages/trainer/pagina-rutinas/pagina-rutinas.component.ts
@@ -3,10 +3,11 @@ import { Trainer } from '../../../services/trainerService/trainer.service';
 import { SidebarDashboardTrainerComponent } from "../../../components/trainer/sidebar-dashboard-trainer/sidebar-dashboard-trainer.component";
 import { Rutina, RutinasService } from '../../../services/rutinasService/rutinas.service';
 import { CardsRutinasComponent } from "../../../components/trainer/cards-rutinas/cards-rutinas.component";
+import { BotonCrearRutinaComponent } from "../../../components/trainer/boton-crear-rutina/boton-crear-rutina.component";
 
 @Component({
   selector: 'app-pagina-rutinas',
-  imports: [SidebarDashboardTrainerComponent, CardsRutinasComponent],
+  imports: [SidebarDashboardTrainerComponent, CardsRutinasComponent, BotonCrearRutinaComponent],
   templateUrl: './pagina-rutinas.component.html',
   styleUrl: './pagina-rutinas.component.css'
 })
