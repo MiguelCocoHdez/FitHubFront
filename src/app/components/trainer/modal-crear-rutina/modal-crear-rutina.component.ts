a3909176746155bad448c2b8793dac6e17d17211
MIGUEL\migue <miguelcocohernandez5@gmail.com>
2025-07-18 02:11:11 +0200

    feat: Creado formulario de crear rutina funcional falta conectarlo a API

--- src/app/components/trainer/modal-crear-rutina/modal-crear-rutina.component.ts
+++ src/app/components/trainer/modal-crear-rutina/modal-crear-rutina.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { Ejercicio } from '../../../services/rutinasService/rutinas.service';
+import { Ejercicio, RutinasService } from '../../../services/rutinasService/rutinas.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-crear-rutina',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './modal-crear-rutina.component.html',
   styleUrl: './modal-crear-rutina.component.css'
 })
@@ -11,11 +12,27 @@ export class ModalCrearRutinaComponent {
 
   @Output() close = new EventEmitter<void>()
 
+  crearRuitinaForm!: FormGroup
   ejercicios: Ejercicio[] = []
 
-  constructor() {}
+  constructor(private fb: FormBuilder, private rutinasService: RutinasService) {
+    this.crearRuitinaForm = this.fb.group({
+      nombreRutina: [''],
+      descripcion: [''],
+      nivel: [''],
+      categoria: [''],
+      duracionMins: [''],
+      ejercicios: this.fb.array(this.ejercicios)
+    })
+  }
 
   cerrarModal() {
     this.close.emit()
+  }
+
+  crearRutina() {
+    if(this.crearRuitinaForm.valid) {
+      console.log(this.crearRuitinaForm.value)
+    }
   }
 }
