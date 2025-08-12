244321c0838609a61228caa5dff6ac165618bf3f
MIGUEL\migue <miguelcocohernandez5@gmail.com>
2025-08-12 03:32:15 +0200

    feat: Pagina de mensajes del trainer carga el trainer

--- src/app/pages/trainer/pagina-mensajes-trainer/pagina-mensajes-trainer.component.ts
+++ src/app/pages/trainer/pagina-mensajes-trainer/pagina-mensajes-trainer.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Trainer } from '../../../services/trainerService/trainer.service';
+import { SidebarDashboardTrainerComponent } from "../../../components/trainer/sidebar-dashboard-trainer/sidebar-dashboard-trainer.component";
 
 @Component({
   selector: 'app-pagina-mensajes-trainer',
-  imports: [],
+  imports: [SidebarDashboardTrainerComponent],
   templateUrl: './pagina-mensajes-trainer.component.html',
   styleUrl: './pagina-mensajes-trainer.component.css'
 })
-export class PaginaMensajesTrainerComponent {
+export class PaginaMensajesTrainerComponent implements OnInit {
+
+  trainer!: Trainer
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.trainer = JSON.parse(localStorage.getItem('trainer') || '{}')
+  }
 
 }
